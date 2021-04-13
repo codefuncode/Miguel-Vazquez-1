025133e4a561bf305c67812e2bd48cec04e82c10
Carlos Aleman <alemandiazcarlos@protonmail.com>
2021-04-13 11:17:34 -0400

    Paguina de resivo en proceso

--- appphp/pag/pago.php
+++ appphp/pag/pago.php
@@ -18,40 +18,45 @@ if (!isset($_COOKIE["ID"])):
       .input-group{
         margin-bottom: 25px !important;
       }
+      .borde{
+        border:5px solid black;
+      }
     </style>
   </head>
   <body>
     <div class="container-fluid" style="padding: 20px 10px 0px 20px;">
-      <div class="row">
-        <div class="col-12" style="margin-bottom: 20px;">
-          <h2>
-            Ingrese los datos de su tarjeta de crédito
-          </h2>
-        </div>
-        <div class="col-6">
-          <label style="display: block !important;">
-            Nombre
-          </label>
-          <div class="input-group">
-            <input aria-label="Text input with dropdown button" class="form-control" type="text"/>
+      <div class="mx-auto" style="width: 900px;">
+        <div class="row">
+          <div class="col-12">
+            <h2>
+              Ingrese los datos de su tarjeta de crédito
+            </h2>
           </div>
-          <label style="display: block !important;">
-            NUmero de la tarjeta
-          </label>
-          <div class="input-group">
-            <input aria-label="Text input with dropdown button" class="form-control" type="text"/>
-          </div>
-          <label style="display: block !important;">
-            Nombre
-          </label>
-          <div class="input-group">
-            <input aria-label="Text input with dropdown button" class="form-control" type="date"/>
-          </div>
-          <label style="display: block !important;">
-            Código de seguridad
-          </label>
-          <div class="input-group">
-            <input aria-label="Text input with dropdown button" class="form-control" type="text"/>
+          <div class="col-12 borde">
+            <label style="display: block !important;">
+              Nombre
+            </label>
+            <div class="input-group">
+              <input aria-label="Text input with dropdown button" class="form-control" type="text"/>
+            </div>
+            <label style="display: block !important;">
+              Número de la tarjeta.
+            </label>
+            <div class="input-group">
+              <input aria-label="Text input with dropdown button" class="form-control" type="text"/>
+            </div>
+            <label style="display: block !important;">
+              Nombre
+            </label>
+            <div class="input-group">
+              <input aria-label="Text input with dropdown button" class="form-control" type="date"/>
+            </div>
+            <label style="display: block !important;">
+              Código de seguridad
+            </label>
+            <div class="input-group">
+              <input aria-label="Text input with dropdown button" class="form-control" type="text"/>
+            </div>
           </div>
         </div>
       </div>
---
+++ appphp/pag/resivo.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+      <link href="https://www.w3schools.com/w3css/4/w3.css" rel="stylesheet"/>
+      <title>
+        Document
+      </title>
+      <style type="text/css">
+        *{
+			box-sizing: border-box;
+			}
+			.borde{
+			border:1px solid grey;
+			border-radius: 10px;
+			}
+			.cabecera{
+
+			height: 200px;
+
+			margin-bottom: 20px;
+			}
+			.cuerpo{
+
+			}
+			.resibo{
+			margin: auto !important; 
+			width: 50%;
+			padding-top: 25px;
+			}
+			.zona_fecha{
+			float: right;
+			}
+			.cuerpo{
+			height: 100px;
+			}
+			.zona_fecha{  clear: both;
+			width:300px;
+		margin: 20px 10px}
+
+		.resibo{
+			background-color:#FFFFCC;
+
+			padding: 25px;
+			border-radius: 15px;
+		
+		}
+      </style>
+    </meta>
+  </head>
+  <body>
+    <div class="w3-container w3-center" style="max-width: 500px; width: 50%;">
+    </div>
+    <div class="" style="margin-top: 100px;">
+      <div class="w3-container w3-center resibo">
+        <div class="">
+          <div class="w3-container cabecera borde">
+            <div class="">
+              <div class="" style="display: inline-block; width: 45%; float: left;">
+                <h2 style="text-align: left;">
+                  GameShark
+                </h2>
+              </div>
+              <div class="" style="display: inline-block; width: 45%; float: right; ">
+                <div class="zona_fecha borde">
+                  Lorem ipsum dolor sit, amet consectetur
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="w3-container cuerpo borde">
+            <div class="">
+              <table style="width:100%">
+                <tr>
+                  <th>
+                    Firstname
+                  </th>
+                  <th>
+                    Lastname
+                  </th>
+                  <th>
+                    Age
+                  </th>
+                </tr>
+                <tr>
+                  <td>
+                    Jill
+                  </td>
+                  <td>
+                    Smith
+                  </td>
+                  <td>
+                    50
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Eve
+                  </td>
+                  <td>
+                    Jackson
+                  </td>
+                  <td>
+                    94
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
